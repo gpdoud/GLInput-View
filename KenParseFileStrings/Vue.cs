bf878f91cbe6688cef047615d49065439f44082c
Greg Doud <gpdoud@gmail.com>
2019-08-22 18:23:08 -0400

    Added ability to trim fields on the left or right side.

--- KenParseFileStrings/Vue.cs
+++ KenParseFileStrings/Vue.cs
@@ -3,41 +3,77 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace KenParseFileStrings {
-    public struct FieldLayout {
-        public string Fieldname { get; set; }
+    public struct LineFmt {
+        public string Field { get; set; }
         public int Start { get; set; }
         public int Len { get; set; }
+        public int LTrim { get; set; }
+        public int RTrim { get; set; }
     }
     public class Vue {
 
-        Dictionary<string, FieldLayout> layout = new Dictionary<string, FieldLayout> {
-            ["company_code"]    = new FieldLayout() { Fieldname = "company_code",     Start = 0,      Len = 5 },
-            ["plan_code"]       = new FieldLayout() { Fieldname = "plan_code",        Start = 5,      Len = 8 },
-            ["source"]          = new FieldLayout() { Fieldname = "source",           Start = 13,     Len = 5 },
-            ["eas_account"]     = new FieldLayout() { Fieldname = "eas_account",      Start = 18,     Len = 10 },
-            ["sap_account"]     = new FieldLayout() { Fieldname = "sap_account",      Start = 28,     Len = 10 },
-            ["profit_center"]   = new FieldLayout() { Fieldname = "profit_center",    Start = 38,     Len = 15 },
-            ["policy_number"]   = new FieldLayout() { Fieldname = "policy_number",    Start = 53,     Len = 15 },
-            ["tran_date"]       = new FieldLayout() { Fieldname = "tran_date",        Start = 68,     Len = 8 },
-            ["calendar_date"]   = new FieldLayout() { Fieldname = "calendar_date",    Start = 76,     Len = 10 },
-            ["ref_id"]          = new FieldLayout() { Fieldname = "ref_id",           Start = 86,     Len = 20 },
-            ["amount"]          = new FieldLayout() { Fieldname = "amount",           Start = 106,    Len = 15 },
-            ["dbcr_ind"]        = new FieldLayout() { Fieldname = "dbcr_ind",         Start = 121,    Len = 1 },
-            ["desc"]            = new FieldLayout() { Fieldname = "desc",             Start = 122,    Len = 50 },
-            ["LOB"]             = new FieldLayout() { Fieldname = "LOB",              Start = 172,    Len = 15 },
-            ["budget_center"]   = new FieldLayout() { Fieldname = "budget_center",    Start = 187,    Len = 15 },
-            ["rein_company"]    = new FieldLayout() { Fieldname = "rein_company",     Start = 202,    Len = 15 },
-            ["state"]           = new FieldLayout() { Fieldname = "state",            Start = 217,    Len = 15 },
-            ["tax_status"]      = new FieldLayout() { Fieldname = "tax_status",       Start = 232,    Len = 15 },
-            ["memo_code"]       = new FieldLayout() { Fieldname = "memo_code",        Start = 247,    Len = 15 }
+        static Dictionary<string, LineFmt> layout = new Dictionary<string, LineFmt> {
+            ["company_code"]    = new LineFmt() { Field = "company_code",  Start = 0,   Len = 5 },
+            ["plan_code"]       = new LineFmt() { Field = "plan_code",     Start = 5,   Len = 8 },
+            ["source"]          = new LineFmt() { Field = "source",        Start = 13,  Len = 5 },
+            ["eas_account"]     = new LineFmt() { Field = "eas_account",   Start = 18,  Len = 10, LTrim = 3 },
+            ["sap_account"]     = new LineFmt() { Field = "sap_account",   Start = 28,  Len = 10, LTrim = 3 },
+            ["profit_center"]   = new LineFmt() { Field = "profit_center", Start = 38,  Len = 15, LTrim = 3 },
+            ["policy_number"]   = new LineFmt() { Field = "policy_number", Start = 53,  Len = 15 },
+            ["tran_date"]       = new LineFmt() { Field = "tran_date",     Start = 68,  Len = 8 },
+            ["calendar_date"]   = new LineFmt() { Field = "calendar_date", Start = 76,  Len = 10 },
+            ["ref_id"]          = new LineFmt() { Field = "ref_id",        Start = 86,  Len = 20 },
+            ["amount"]          = new LineFmt() { Field = "amount",        Start = 106, Len = 15 },
+            ["dbcr_ind"]        = new LineFmt() { Field = "dbcr_ind",      Start = 121, Len = 1 },
+            ["desc"]            = new LineFmt() { Field = "desc",          Start = 122, Len = 50 },
+            ["LOB"]             = new LineFmt() { Field = "LOB",           Start = 172, Len = 15 },
+            ["budget_center"]   = new LineFmt() { Field = "budget_center", Start = 187, Len = 15 },
+            ["rein_company"]    = new LineFmt() { Field = "rein_company",  Start = 202, Len = 15 },
+            ["state"]           = new LineFmt() { Field = "state",         Start = 217, Len = 15 },
+            ["tax_status"]      = new LineFmt() { Field = "tax_status",    Start = 232, Len = 15 },
+            ["memo_code"]       = new LineFmt() { Field = "memo_code",     Start = 247, Len = 15 }
         };
 
-        private string ParseField(string line, string fieldname,  Dictionary<string, FieldLayout> layout) {
+        /// <summary>
+        /// Extracts fields from the input string using parameters from the Dictionary which contains the 
+        /// starting position of the field and the number of characters. It also allows for optionally
+        /// trimming characters from the left and/or right of the extracted field.
+        /// 
+        /// Trimming characters is required to remove leading zeros on some account numbers. For this
+        /// to operate properly, the right side it trimmed first then the left. It must be done in 
+        /// this order.
+        /// </summary>
+        /// 
+        /// <param name="line">
+        /// This is a line from the input file. It is a single string with no
+        /// delimiters. All fields are positional.
+        /// </param>
+        /// 
+        /// <param name="fieldname">
+        /// The name of the field to be extracted. It must be a key in 
+        /// the dictionary.
+        /// </param>
+        /// 
+        /// <param name="layout">
+        /// The dictionary containing the line layout info.
+        /// </param>
+        /// 
+        /// <returns>
+        /// A field as a string extracted from the input.
+        /// </returns>
+        private string ParseField(string line, string fieldname,  Dictionary<string, LineFmt> layout) {
             var fieldLayout = layout[fieldname];
             if(fieldLayout.Start + fieldLayout.Len > line.Length) {
                 return string.Empty;
             }
-            return line.Substring(fieldLayout.Start, fieldLayout.Len).Trim();
+            var field = line.Substring(fieldLayout.Start, fieldLayout.Len).Trim();
+            if(fieldLayout.RTrim > 0) {
+                field = field.Substring(0, field.Length - fieldLayout.RTrim);
+            }
+            if(fieldLayout.LTrim > 0) {
+                field = field.Substring(fieldLayout.LTrim);
+            }
+            return field;
         }
 
         private GLInput ParseLine(string line) {
